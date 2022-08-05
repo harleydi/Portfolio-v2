58a7fd0915bec04f1178f661aec4dc49a4b50e94
Disney Harley <disneyharley26@gmail.com>
2022-08-05 19:14:57 -0400

    added home button working

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="info">
         <p className="nameTitle">Disney Harley</p>
         <h5 className="roleTitle">Full-Stack Developer</h5>
-        <a href="/work"><button>PROJECTS</button></a>
+        <a href="\work"><button>PROJECTS</button></a>
       </div>
     </div>
   );
